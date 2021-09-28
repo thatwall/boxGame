5d282406c30ff39a6eed7e5cf3bc64ced649fe00
oyz <oyz@LAPTOP-S071DNT9>
2021-09-28 13:23:48 +0800

    通过测试样例初级49

--- boxGame/Box.h
+++ boxGame/Box.h
@@ -1,30 +1,39 @@
 #pragma once
+#include <queue>
+#include <set>
+#include <sstream>
 #include <string>
 #include <vector>
 using namespace std;
 
-string test1() { return "Hello"; }
+string test1() {
+  return "Hello";
+}
 
 struct point {
   int x, y;
+  point() : x(0), y(0) {}
   point(int _x, int _y) : x(_x), y(_y) {}
   point operator+(const point& pt) { return point(x + pt.x, y + pt.y); }
+  point operator-(const point& pt) { return point(x - pt.x, y - pt.y); }
   bool operator==(const point& pt) { return x == pt.x && y == pt.y; }
 };
 const point dirs[] = {point(0, 1), point(1, 0), point(0, -1), point(-1, 0)};
 
 // 'b'Ïä×Ó£¬'w'Ç½£¬'.'¿Õ°×
-enum atom { box = 'B', wall = 'W', blank = '.' };
+enum atom { box = 'b', wall = 'w', blank = '.' };
 enum direction { east, south, west, north };
 
 struct boxState {
-  vector<vector<char>> board;
+  vector<string> board;
   point cur;
-  vector<point> targets;
-
+  static vector<point> targets;
+  vector<point> ops;
+  boxState() : cur(-1, -1) {}
+  bool operator==(boxState& bs) { return index() == bs.index(); }
   char& getAtom(point pt) { return board[pt.x][pt.y]; }
-  bool forward(int d) {
-    point dir = dirs[d];
+  void forward(const point& dir) {
+    // point dir = dirs[d];
     point dest = cur + dir;
     if (getAtom(dest) == atom::box) {
       if (getAtom(dest + dir) == atom::blank) {
@@ -35,11 +44,22 @@ struct boxState {
     } else if (getAtom(dest) == atom::blank) {
       cur = dest;
     }
-    return cur == dest;
+    if (cur == dest) {
+      ops.push_back(dir);
+    }
+    //return cur == dest;
   }
-  void backward(int d) {
+  void backward() {
+    auto lastOp = ops.back();
+    ops.pop_back();
+    auto dest = cur + lastOp;
+    if (getAtom(dest) == atom::box) {
+      getAtom(cur) = atom::box;
+      getAtom(dest) = atom::blank;
+    }
+    cur = cur - lastOp;
+  }
 
-  }
   bool isFin() {
     for (const point& pt : targets) {
       if (getAtom(pt) != atom::box) {
@@ -48,4 +68,43 @@ struct boxState {
     }
     return true;
   }
+  string toString() {
+    getAtom(cur) = 'P';
+    string res;
+    for (int i = 0; i < board.size(); i++) {
+      res += board[i] + '\n';
+    }
+    getAtom(cur) = '.';
+    return res;
+  }
+  string index() {
+    return toString();
+    //return toString() + to_string(cur.x) + to_string(cur.y);
+  }
 };
+
+vector<point> boxState::targets = vector<point>();
+
+boxState bfs(boxState st) {
+  static queue<boxState> q;
+  static set<string> s;
+  q.push(st);
+  s.insert(st.index());
+  while (!q.empty()) {
+    auto top = q.front();
+    q.pop();
+    if (top.isFin()) {
+      return top;
+    }
+    for (int i = 0; i < 4; i++) {
+      auto temp = top;
+      temp.forward(dirs[i]);
+      if (!s.count(temp.index())) {
+        q.push(temp);
+        s.insert(temp.index());
+      }
+
+    }
+  }
+  return boxState();
+}
--- boxGame/boxGame.cpp
+++ boxGame/boxGame.cpp
@@ -2,18 +2,61 @@
 //
 
 #include <iostream>
-#include <string>
+#include <fstream>
 #include "Box.h"
 
-int main() { std::cout << test1() << std::endl; }
+int main() {
+  //FILE* pFile;
+  //pFile = fopen("./input.txt", "r");
+  //if (pFile == NULL) {
+  //  printf("Error opening file unexist.ent: %s\n", strerror(errno));
+  //  return -1;
+  //}
+  ifstream fin("./input.txt");
+  if (!fin.is_open()) {
+    cerr << "failed" << endl;
+    return -1;
+  }
+  boxState bs;
+  fin >> bs.cur.x >> bs.cur.y;
+  int tnum = 0;
+  fin >> tnum;
+  while (tnum--) {
+    point tmp;
+    fin >> tmp.x >> tmp.y;
+    bs.targets.push_back(tmp);
+  }
+  string line;
+  while (fin >> line) {
+    bs.board.push_back(line);
+  }
+  auto result = bfs(bs);
+  if (!(result.cur == point(-1, -1))) {
+    for (int i = 0; i < result.ops.size(); i++) {
+      if (result.ops[i] == point(0, 1)) {
+        cout << "→" << endl;
+      } else if (result.ops[i] == point(1, 0)) {
+        cout << "↓" << endl;
+      } else if (result.ops[i] == point(0, -1)) {
+        cout << "←" << endl;
+      } else if (result.ops[i] == point(-1, 0)) {
+        cout << "↑" << endl;
+      }
+      bs.forward(result.ops[i]);
+      cout << bs.toString() << endl;
+    }
+  }
+  return 0;
+}
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
 // 调试程序: F5 或调试 >“开始调试”菜单
 
-// 入门使用技巧: 
+// 入门使用技巧:
 //   1. 使用解决方案资源管理器窗口添加/管理文件
 //   2. 使用团队资源管理器窗口连接到源代码管理
 //   3. 使用输出窗口查看生成输出和其他消息
 //   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   5.
+//   转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
 //   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
